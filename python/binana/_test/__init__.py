4ef4d88e2f8e94ae5369fe61f25e9c8e60f0ae00
jdurrant <jacobdurrant@gmail.com>
2021-11-30 01:59:46 -0500

    Minor fix to tests.

--- python/binana/_test/__init__.py
+++ python/binana/_test/__init__.py
@@ -24,6 +24,9 @@ def _remove_lines_with_pass(txt):
     ]:
         txt = re.sub(r"^REMARK +?" + val + r".+?\n", "", txt, flags=re.M | re.S)
         txt = re.sub(r"^ +?" + val + r".+?\n", "", txt, flags=re.M | re.S)
+
+        # "Rounds" numbers (to account for minor system differences)
+        txt = re.sub(r"([0-9]\.[0-9]{4})[0-9]{1,15},", r"\1", txt)
     return txt
 
 
